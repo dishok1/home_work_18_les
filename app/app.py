e0e6d57ba8cad6d8da4b89aec05d4e8ac63a3fbd
Dima Pan <dimdish4@gmail.com>
2026-08-13 19:02:34 +0300

    Оновлено залежності та виправлено імпорти

--- app/app.py
+++ app/app.py
@@ -1,23 +1,12 @@
 from fastapi import Depends, FastAPI
-from contextlib import asynccontextmanager  
 
 from app.db import User, create_db_and_tables
 from app.schemas import UserCreate, UserRead, UserUpdate
 from app.users import auth_backend, current_active_user, fastapi_users
 from app.files import file_router
 
+app = FastAPI()
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    
-    await create_db_and_tables()
-    yield
-    
-
-
-app = FastAPI(lifespan=lifespan)
-
-# --- ВАШІ РОУТЕРИ (ЗАЛИШАЮТЬСЯ БЕЗ ЗМІН) ---
 app.include_router(
     fastapi_users.get_auth_router(auth_backend), prefix="/auth/jwt", tags=["auth"]
 )
@@ -41,6 +30,7 @@ app.include_router(
     prefix="/users",
     tags=["users"],
 )
+
 app.include_router(
     file_router,
     prefix="/files",
@@ -51,3 +41,8 @@ app.include_router(
 async def authenticated_route(user: User = Depends(current_active_user)):
     return {"message": f"Hello {user.email}!"}
 
+
+@app.on_event("startup")
+async def on_startup():
+    # Not needed if you setup a migration system like Alembic
+    await create_db_and_tables()
